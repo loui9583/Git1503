040fa76325ae18d0832fb1832a3102bef1814a80
louis <loui9583@stud.kea.dk>
2023-03-15 08:54:35 +0100

    Initial commit

---
+++ src/main/java/com/example/git1503/Git1503Application.java
@@ -0,0 +1,13 @@
+package com.example.git1503;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1503Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git1503Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git1503/Git1503ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git1503;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git1503ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
